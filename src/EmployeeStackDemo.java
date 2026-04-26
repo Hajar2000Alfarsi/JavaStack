fe1f2f9ca087597694c254d1ce0a9c9a4977accd
Hajar <hajar2000alfarsi@gmail.com>
2026-04-26 19:31:40 +0400

    Add employee action records using push()

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -4,6 +4,10 @@ public class EmployeeStackDemo {
     public static void mani(String[] args){
         Stack<String> employeeAction= new Stack<>();
 
-
+        employeeAction.push("Employee A Logged in");
+        employeeAction.push("Employee B uploaded a file");
+        employeeAction.push("Employee C updated a record");
+        employeeAction.push("Employee D logged out");
+        employeeAction.push("Employee E changed password");
     }
 }
