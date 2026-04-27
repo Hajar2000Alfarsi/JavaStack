b917c9e0fffbc71d8864c7759c8b25f2f013bcf5
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 08:16:23 +0400

    Visit google.com

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -3,5 +3,10 @@ import java.util.Stack;
 public class BrowserHistorySimulator {
     public static void main(String[] args) {
         Stack<String> webpageUrl= new Stack<>();
+
+        webpageUrl.push("goole,com");
+        System.out.println("Current page: " + webpageUrl.peek());
+        System.out.println("History: " + webpageUrl);
+
     }
 }
