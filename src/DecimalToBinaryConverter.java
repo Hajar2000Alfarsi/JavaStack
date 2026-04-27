748f51ff297eecacd7a51d95c699abfdeea8aa61
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 11:05:20 +0400

    Implement decimalToBinary method

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -1,6 +1,10 @@
 import java.util.Stack;
 public class DecimalToBinaryConverter {
     public static void main(String[] args) {
+        int[] testNumbers= {0, 1, 5, 10, 25, 100};
+    }
+
+    public String decimalToBinary(int decimal){
 
     }
 }
