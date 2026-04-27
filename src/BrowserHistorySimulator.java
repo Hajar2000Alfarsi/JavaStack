3fc3f3e9c4d4aaa4014e1ca7aad80046cfeab8a2
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 08:20:05 +0400

    Visit github.com

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -5,12 +5,17 @@ public class BrowserHistorySimulator {
         Stack<String> webpageUrl= new Stack<>();
 
         webpageUrl.push("goole,com");
-        System.out.println("Visited goole.com");
+        System.out.println("\nVisited goole.com");
         System.out.println("Current page: " + webpageUrl.peek());
         System.out.println("History: " + webpageUrl);
 
         webpageUrl.push("youtube.com");
-        System.out.println("Visited youtube.com");
+        System.out.println("\nVisited youtube.com");
+        System.out.println("Current page: " +webpageUrl.peek());
+        System.out.println("History: " + webpageUrl);
+
+        webpageUrl.push("github.com");
+        System.out.println("\nVisited github.com");
         System.out.println("Current page: " +webpageUrl.peek());
         System.out.println("History: " + webpageUrl);
     }
