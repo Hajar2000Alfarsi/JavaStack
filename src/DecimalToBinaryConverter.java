8e46b2dcad8312ca48d243cb4f07d70b6260d8ca
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 11:17:27 +0400

    Use while loop to calculate the reminder

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -5,11 +5,22 @@ public class DecimalToBinaryConverter {
     }
 
     public String decimalToBinary(int decimal){
+        //if decimal=0  return 0
         if (decimal == 0){
             return "0";
         }
 
         Stack<Integer> num= new Stack<>();
+        int temp = decimal;
+        //Calculate the reminder
+        while (temp > 0){
+            int reminder = temp % 2;
+            num.push(reminder);
+            temp = temp / 2;
+            System.out.println("Push remainder: " + reminder + " --> " + num);
+
+        }
+
 
     }
 }
