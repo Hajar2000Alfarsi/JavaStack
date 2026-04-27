07656dbd7fb0a100d0507be6730214ffa502846d
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 14:34:38 +0400

    If undo exceeds 10 delete first one

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -3,6 +3,15 @@ public class TextEditorWithUndoRedo {
     Stack<String> undoStack = new Stack<>();
     Stack<String> redoStack = new Stack<>();
     String text = "";
+
+    public void type(String inputText) {
+        undoStack.push(text);
+
+        //If undo exceeds 10 delete first one
+        if (undoStack.size() > 10){
+            undoStack.remove(0);
+        }
+    }
     public static void main(String[] args){
 
 
