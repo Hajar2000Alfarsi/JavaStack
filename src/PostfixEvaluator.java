55c12836ebee4cec4bbdeb6a06cf1eaa997ee611
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 12:09:34 +0400

    Create stack and array String to spilt the expression to take only numbers

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -1,9 +1,16 @@
+import java.util.Stack;
+
+import static jdk.nashorn.internal.runtime.JSType.isNumber;
+
 public class PostfixEvaluator {
     public static void main(String[] args) {
         String[] postfix= {"3 4 +", "10 2 /", "5 1 2 + 4 * + 3 -", "6 2 * 3 /", "8 3 %"};
     }
 
     public int evaluatePostfix(String expression) {
+        Stack<Integer> stack = new Stack<>();
+        //split expression by " " to take only numbers
+        String[] tokens= expression.split(" ");
 
     }
 }
