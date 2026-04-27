4664babc4ff08461d80e3dd813add29e94e6aa47
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 08:18:51 +0400

    Visit youtube.com

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -5,8 +5,13 @@ public class BrowserHistorySimulator {
         Stack<String> webpageUrl= new Stack<>();
 
         webpageUrl.push("goole,com");
+        System.out.println("Visited goole.com");
         System.out.println("Current page: " + webpageUrl.peek());
         System.out.println("History: " + webpageUrl);
 
+        webpageUrl.push("youtube.com");
+        System.out.println("Visited youtube.com");
+        System.out.println("Current page: " +webpageUrl.peek());
+        System.out.println("History: " + webpageUrl);
     }
 }
