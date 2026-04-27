13dc5324f65eb0cd3f1d12792ef5c8fa15ff3bf8
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 11:43:06 +0400

    show the process of converting decimal to binary

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Stack;
 public class DecimalToBinaryConverter {
     public static void main(String[] args) {
@@ -7,7 +8,10 @@ public class DecimalToBinaryConverter {
 
         for (int numbers: testNumbers){
             System.out.println("\nDecimal: " + numbers);
-            System.out.println("Binary Number: " +testBinary.decimalToBinary(numbers));
+            System.out.println("Binary Number(stack): " +testBinary.decimalToBinary(numbers));
+            System.out.println("\nBinary Number(Built-in): " );
+            testBinary.displayConversionProcess(numbers);
+
         }
     }
 
@@ -25,7 +29,6 @@ public class DecimalToBinaryConverter {
             num.push(reminder);
             temp = temp / 2;
             System.out.println("Push remainder: " + reminder + " --> " + num);
-
         }
 
         //build the binary number from the stack
@@ -37,6 +40,20 @@ public class DecimalToBinaryConverter {
     }
 
     public void displayConversionProcess(int decimal){
+        //if decimal=0  return 0
+        if (decimal == 0) {
+            System.out.println("0 = 0");
+        }
 
+        Stack<Integer> stack= new Stack<>();
+        int temp = decimal;
+        //Calculate the reminder in detail
+        while (temp > 0){
+            int reminder = temp % 2;
+            System.out.println(temp + "÷2 = " + temp/2 + " remainder --> " + reminder);
+            stack.push(reminder);
+            temp = temp / 2;
+        }
+        System.out.println("Stack: " + stack);
     }
 }
