61449d7de6afe0cf4d4d4b8c8612c19d770f8b7b
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 09:57:19 +0400

    Create a String for testing

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class StringReversalAndPalindrome {
     public static void main(String[] args){
+        String[] testCases= {"hello", "madam", "racecar", "java", "a", ""};
 
     }
 }
