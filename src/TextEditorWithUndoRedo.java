d4cc2ed4fdce24b68fab20bf7f76f8e9afa9e9ba
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 14:25:45 +0400

    create stack for undo and redo

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,6 +1,10 @@
 import java.util.Stack;
 public class TextEditorWithUndoRedo {
+    Stack<String> undoStack = new Stack<>();
+    Stack<String> redoStack = new Stack<>();
+    String text = "";
     public static void main(String[] args){
+
 
     }
 }
