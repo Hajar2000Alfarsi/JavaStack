88ee3077ff485bee5b94c9cb9bec79ed2bf7a11a
Hajar <hajar2000alfarsi@gmail.com>
2026-04-27 12:03:33 +0400

    Implement evaluatePostfix method

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -2,4 +2,8 @@ public class PostfixEvaluator {
     public static void main(String[] args) {
         String[] postfix= {"3 4 +", "10 2 /", "5 1 2 + 4 * + 3 -", "6 2 * 3 /", "8 3 %"};
     }
+
+    public int evaluatePostfix(String expression) {
+
+    }
 }
