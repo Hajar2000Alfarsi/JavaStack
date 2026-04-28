d9fe4c1ff029ca37e5605cf89a648a2983b33923
Hajar <hajar2000alfarsi@gmail.com>
2026-04-28 08:16:06 +0400

    Add a 8 diffrent HTML/XML for testing

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -2,6 +2,10 @@ import java.util.Stack;
 public class HtmlTagValidator {
     public static void main(String[] args) {
         String[] tests= {"<div><p>Hello</p></div>", "<div><p></div></p>",
-                       "<b><i>text</i></b>", "<div><br/></div>", "<html><body></body></html>"};
+                       "<b><i>text</i></b>", "<div><br/></div>",
+                "<html><body></body></html>", "<div><p>Test</p>",
+                "<div><span></div></span>", "<h1>Title</h1><p>Paragraph</p>"};
     }
+
+
 }
