43ea402829dcee68db773bdf75d2f9da63100c56
Hajar <hajar2000alfarsi@gmail.com>
2026-04-28 14:11:54 +0400

    Create 3 stacks

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -1,5 +1,10 @@
 import java.util.Stack;
 public class TowerOfHanoiSolver {
+    //create 3 stacks
+    static Stack<Integer> A = new Stack<>();
+    static Stack<Integer> B = new Stack<>();
+    static Stack<Integer> C = new Stack<>();
+
     public static void main(String[] args) {
 
     }
