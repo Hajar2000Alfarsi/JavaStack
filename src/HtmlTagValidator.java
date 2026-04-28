2e0c506d2c11782e210c7fb252b3012f824de2f7
Hajar <hajar2000alfarsi@gmail.com>
2026-04-28 08:13:53 +0400

    Add a string for testing

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class HtmlTagValidator {
     public static void main(String[] args) {
-
+        String[] tests= {"<div><p>Hello</p></div>", "<div><p></div></p>",
+                       "<b><i>text</i></b>", "<div><br/></div>", "<html><body></body></html>"};
     }
 }
