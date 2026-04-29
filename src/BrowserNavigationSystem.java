1cc632d237b397ad351cb2ccc0abe9296440f22a
Hajar <hajar2000alfarsi@gmail.com>
2026-04-29 10:42:22 +0400

    variable to store the current page

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -3,6 +3,9 @@ public class BrowserNavigationSystem {
     //Create stacks: one for back, one for forward
     static Stack<String> backStack= new Stack<>();
     static Stack<String> forwardStack = new Stack<>();
+
+    //variable to store the current page
+    String currentPage= "";
     public static void main(String[] args){
 
     }
