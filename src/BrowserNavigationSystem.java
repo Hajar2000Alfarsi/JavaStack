ebee8db53034b2e0cdbbaac0ab26864f090ef252
Hajar <hajar2000alfarsi@gmail.com>
2026-04-29 10:40:58 +0400

    Create stacks: one for back, one for forward

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -1,5 +1,8 @@
 import java.util.Stack;
 public class BrowserNavigationSystem {
+    //Create stacks: one for back, one for forward
+    static Stack<String> backStack= new Stack<>();
+    static Stack<String> forwardStack = new Stack<>();
     public static void main(String[] args){
 
     }
