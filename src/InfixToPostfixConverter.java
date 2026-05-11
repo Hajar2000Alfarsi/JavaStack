5a8290916a96b869ee0378c83498311cb397fe32
Hajar <hajar2000alfarsi@gmail.com>
2026-05-11 21:18:20 +0400

    return precedence of operators

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -7,7 +7,24 @@ public class InfixToPostfixConverter {
     }
 
     public static int getPrecedence(char operator){
+        switch (operator) {
 
+            case '^':
+                return 3;
+
+            case '*':
+            case '/':
+            case '%':
+                return 2;
+
+            case '+':
+            case '-':
+                return 1;
+
+            default:
+                return -1;
+        }
+    }
     }
 
     public static void main(String[] args) {
