30f8662e2d12f7f49b34664e08c7330d4337525d
Hajar <hajar2000alfarsi@gmail.com>
2026-05-12 21:24:06 +0400

    find the next smaller element

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -58,6 +58,28 @@ public class NextGreaterElementFinder {
         if (arr == null || arr.length == 0) {
             return new int[0];
         }
+
+        int n = arr.length;
+
+        int[] result = new int[n];
+
+        Arrays.fill(result, -1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && arr[i] < arr[stack.peek()]) {
+
+                int index = stack.pop();
+
+                result[index] = arr[i];
+            }
+
+            stack.push(i);
+        }
+
+        return result;
     }
 }
 
