5a04537c6f7562a675a6ededd0cb311f4dc30a6f
Hajar <hajar2000alfarsi@gmail.com>
2026-05-12 21:49:51 +0400

    Constructor

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -1,4 +1,12 @@
+import java.util.Stack;
+
 public class QueueUsingTwoStacks {
     private Stack<T> inputStack;
     private Stack<T> outputStack;
+
+    public QueueUsingTwoStacks() {
+
+        inputStack = new Stack<>();
+        outputStack = new Stack<>();
+    }
 }
