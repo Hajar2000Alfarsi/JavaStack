80e468fc76de3b401a6a4f82ce22b4b39ce44960
Hajar <hajar2000alfarsi@gmail.com>
2026-05-13 21:41:33 +0400

    Min stack stores minimum values

--- src/MinStack.java
+++ src/MinStack.java
@@ -4,4 +4,6 @@ public class MinStack {
     // Main stack stores all elements
     private Stack<Integer> mainStack;
 
+    // Min stack stores minimum values
+    private Stack<Integer> minStack;
 }
