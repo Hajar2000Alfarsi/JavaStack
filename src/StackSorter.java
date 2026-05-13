5715ca5e7e86a618566557c8699199cd8c820dfe
Hajar <hajar2000alfarsi@gmail.com>
2026-05-13 21:57:53 +0400

    Temporary stack

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -3,6 +3,9 @@ import java.util.Stack;
 public class StackSorter {
     // Iterative method using temporary stack
     public static Stack<Integer> sortStack(Stack<Integer> stack) {
+        // Temporary stack
+        Stack<Integer> tempStack = new Stack<>();
 
+        System.out.println("\n=== ITERATIVE SORTING PROCESS ===");
     }
 }
