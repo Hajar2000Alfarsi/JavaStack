6aec3dc0cb46d5b9d2ec6a17d9028733f95517ce
Hajar <hajar2000alfarsi@gmail.com>
2026-05-16 18:00:15 +0400

    Stack for operators

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -2,4 +2,7 @@ import java.util.Stack;
 
 public class InfixExpressionEvaluator {
     static Stack<Double> operandStack = new Stack<>();
+
+    // Stack for operators
+    static Stack<Character> operatorStack = new Stack<>();
 }
