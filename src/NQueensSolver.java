e610a1983957ab5cdb3a70dfa268a561148fd77b
Hajar <hajar2000alfarsi@gmail.com>
2026-05-16 18:44:55 +0400

    Constructor

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -4,5 +4,16 @@ public class NQueensSolver {
 
         int row;
         int col;
+
+        // Constructor
+        public Position(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + row + "," + col + ")";
+        }
     }
 }
