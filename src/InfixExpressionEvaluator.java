48e4a3c025e81209873da89d6f1462130134469e
Hajar <hajar2000alfarsi@gmail.com>
2026-05-16 18:01:13 +0400

    Array of test expressions

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -7,6 +7,20 @@ public class InfixExpressionEvaluator {
     static Stack<Character> operatorStack = new Stack<>();
 
     public static void main(String[] args) {
-
+// Array of test expressions
+        String[] expressions = {
+                "5 + 3",
+                "10 + 2 * 6",
+                "100 * 2 + 12",
+                "100 * ( 2 + 12 )",
+                "100 * ( 2 + 12 ) / 14",
+                "5 + 2 * 3 - 1",
+                "10 / 2 + 7",
+                "2 ^ 3",
+                "20 % 3",
+                "3.5 + 2.1",
+                "-5 + 3",
+                "( 8 + 2 ) * ( 3 - 1 )"
+        };
     }
 }
