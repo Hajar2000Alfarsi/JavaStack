cc328e9826434daeac3a10cf8af816c34047eb3e
Hajar <hajar2000alfarsi@gmail.com>
2026-05-16 18:21:48 +0400

    Create main method

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -4,5 +4,7 @@ public class LargestRectangleInHistogram {
     // Stack to store indices of histogram bars
     static Stack<Integer> stack = new Stack<>();
 
+    public static void main(String[] args) {
 
+    }
 }
