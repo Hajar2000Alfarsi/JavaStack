d195242c309b7dcd51051c85e849ad5ebb9aeb3b
Hajar <hajar2000alfarsi@gmail.com>
2026-05-16 18:22:32 +0400

    Different histogram test cases

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -5,6 +5,16 @@ public class LargestRectangleInHistogram {
     static Stack<Integer> stack = new Stack<>();
 
     public static void main(String[] args) {
-
+        // Different histogram test cases
+        int[][] testCases = {
+                {2, 1, 5, 6, 2, 3},
+                {2, 4},
+                {6, 2, 5, 4, 5, 1, 6},
+                {1, 1, 1, 1},
+                {5, 4, 3, 2, 1},
+                {1, 2, 3, 4, 5},
+                {7},
+                {}
+        };
     }
 }
